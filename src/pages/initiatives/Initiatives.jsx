40b92ebe0e8c539661490b3e00b8e3ef7cdef811
Subodh <subodhjajee123@gmail.com>
2023-11-13 21:44:46 +0530

    initiatives page

--- src/pages/initiatives/Initiatives.jsx
+++ src/pages/initiatives/Initiatives.jsx
@@ -1,11 +1,32 @@
+// Initiatives.js
 
 import React from 'react';
 import './Initiatives.css';
+import { initiatives } from './info';
 
 const Initiatives = () => {
   return (
-    <div>
-        <h2>Initiatives</h2>
+    <div className="initiatives-page">
+      <div className="initiatives-container">
+        <h2>Significant Initiatives</h2>
+        {initiatives.map((initiative, index) => (
+          <div key={index} className="initiative-card">
+            <h3>{initiative.name}</h3>
+            <div className="initiative-details">
+              <div className="initiative-details-left">
+                <p dangerouslySetInnerHTML={{ __html: initiative.description }}></p>
+                <div className="initiative-links">
+                  {initiative.facebook && <a href={initiative.facebook} target="_blank" rel="noopener noreferrer">Facebook</a>}
+                  {initiative.website && <a href={initiative.website} target="_blank" rel="noopener noreferrer">Website</a>}
+                </div>
+              </div>
+              <div className="initiative-details-right">
+                <img src={initiative.photo} alt={initiative.name} className="initiative-photo" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/pages/initiatives/info.js
@@ -0,0 +1,65 @@
+export const initiatives = [
+    {
+        "name": "The Gender Park",
+        "description": "An innovative initiative of the Department of Social Justice, Government of Kerala, India, aiming to create a unifying platform for gender equity.<br><b>Adress: </b>A-17 Brahmins Colony Lane Thiruvananthapuram, India 695005",
+        "facebook": "https://www.facebook.com/thegenderpark/",
+        "website": "http://www.genderpark.gov.in/",
+        "photo": "https://lgbtqindiaresource.files.wordpress.com/2020/09/gender-park.jpg?w=300"
+    },
+    {
+        "name": "Samanwaya Programme",
+        "description": "A first of its kind in the country launched by Kerala's state Literacy Mission, as many as 145 transgenders are attending classes across the state and are preparing for the Classes IV, VII, X and higher secondary equivalency exams.",
+        "website": "https://www.newindianexpress.com/cities/thiruvananthapuram/2018/feb/23/samanwaya-programme-scripting-a-new-chapter-for-transgenders-1777566.html",
+        "photo": "samanwaya.jpeg"
+    },
+    {
+        "name": "Kudumbashree Ayalkoottam",
+        "description": "In an effort to bring transgender persons in the district together and encourage them to take up entrepreneurial ventures, the Malappuram district Kudumbashree Mission has launched two exclusive units popularly called as Ayalkoottam (neighbourhood group). The mission provides awareness and skill training to equip them to launch their own ventures.",
+        "website": "https://www.deccanchronicle.com/nation/current-affairs/190517/kudumbashree-forms-groups-for-transgenders.html",
+        "photo": "kumb.png"
+    },
+    {
+        "name": "QAMRA",
+        "description": "The Queer Archive for Memory Reflection and Activism, a multimedia archival project aiming to chronicle and preserve stories of marginalized communities based on gender and sexuality in India.",
+        "website": "https://qamra.in/",
+        "photo": "https://lgbtqindiaresource.files.wordpress.com/2020/09/queer-archive.png"
+    },
+    {
+        "name": "Pink List India",
+        "description": "Pink List India is the country's first archive of politicians supporting LGBTQIA+ rights. Situated at the intersection of politics, queerness, and technology, we aim to push for accountability and transparency from our elected representatives.",
+        "website": "https://www.pinklistindia.com/",
+        "facebook": "https://twitter.com/pinklistindia",
+        "photo": "https://lgbtqindiaresource.files.wordpress.com/2020/09/pink-list-india.jpg?w=300"
+    },
+    {
+        "name": "Safe Access",
+        "description": "A community-led initiative by Qequal Foundation, based in Delhi, working at the intersection of health and technology to enable equitable healthcare for the LGBTQ+ community in India.",
+        "facebook": "https://www.facebook.com/followsafeaccess/",
+        "website": "http://www.safeaccess.co.in",
+        "photo": "https://scontent.famd15-1.fna.fbcdn.net/v/t39.30808-6/310546362_474716524694060_8499760303691289942_n.png?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=EdxEGMwf5w4AX956OTk&_nc_ht=scontent.famd15-1.fna&oh=00_AfC_qZxY9gPzkFMuwSR47dkF7v3GGkPLPzbK1Xxf2u-mXg&oe=65574059"
+    },
+    {
+        "name": "Bold Transgender Modeling Agency",
+        "description": "Founded by Rudrani Chettri, a Delhi-based transgender activist and head of Mitr Trust, to empower young transgender individuals through modeling.",
+        "facebook": "https://www.facebook.com/boldtransmodels/",
+        "photo": "https://lgbtqindiaresource.files.wordpress.com/2020/09/bold.png?w=300"
+    },
+    {
+        "name": "GHAR घर Gay Housing Assistance Resource گھر",
+        "description": "G.H.A.R., (Gay Housing Assistance Resource) which means 'home' in many Indian languages. It is a FREE all-India location-based, short-, medium- and long-term accommodation resource bulletin board for LGBTIQ people and allies who are or plan to be renters, roommates, flatmates or owners in India. Keeping up with the evolution of the internet,the group has moved from started on E-groups (1998), moved to Yahoogroups (2001) and then to Facebook (2012).",
+        "website": "Started on E-groups (1998), moved to Yahoogroups (2001), and then to Facebook (2012)",
+        "photo": "https://scontent.famd15-2.fna.fbcdn.net/v/t39.30808-6/318065589_10159325159021045_8357109407285708203_n.png?stp=dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=CSuCG02Q3QYAX92khMS&_nc_ht=scontent.famd15-2.fna&oh=00_AfAcfNEIzewXRmdGAvD3dN9MSH8Prf0Q1S3DHKKqfnGkBw&oe=6557E9FA"
+    },
+    {
+        "name": "101 Coming Out Stories from India",
+        "description": "It publishes coming out stories from members of LGBTAIQ+ community & Allies from India. Its voices of individuals from diverse backgrounds on their story of being their authentic self.",
+        "website": "http://101comingoutstories.in/",
+        "photo": "https://ourqueerstories.s3.amazonaws.com/wp-content/uploads/2015/11/our-queer-stories-banner.jpg"
+    },
+    {
+        "name": "Bindu Queer Seed Fund Initiative",
+        "description": "Initiated by Bindumadhav Khire, founder of Samapathik Trust, providing small grants to organizations or groups working on LGBTI empowerment in India. <br><b>Grant Amount: </b>Minimum of 1 lakh Rs. every year for a minimum of 15 years",
+        "facebook": "https://www.facebook.com/140061432779100/photos/a.145567445561832.28795.140061432779100/1328510180600880/",
+        "photo": "https://lgbtqindiaresource.files.wordpress.com/2020/09/bmk-1.jpg?w=385"
+    },
+]
